500e20db298b1597a87e49083f753e62e5a931c1
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-10-05 16:27:24 +0800

    new-accordion

--- scripts.js
+++ scripts.js
@@ -8,15 +8,15 @@ $(document).ready(function() {
 
 
 
-var allPanels = $('.accordion > dd').hide();
+var allPanels = $('.accordion > .dd').hide();
 
-	$('.accordion > dt > a').click(function() {
+	$('.accordion > .dt > a').click(function() {
 		allPanels.slideUp();
 
     $(this).filter('.open').removeClass('open').addClass('closing')
-    $('.accordion > dt > a').removeClass('open');
+    $('.accordion > .dt > a').removeClass('open');
   $(this).not(".closing").addClass('open').parent().next().slideDown();
-    $('.accordion > dt > a').removeClass('closing');
+    $('.accordion > .dt > a').removeClass('closing');
     
     return false;
 	});
