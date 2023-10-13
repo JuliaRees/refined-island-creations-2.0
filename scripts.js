4488437e4dc9d3852e482b40d66532f19e4c13fe
JuliaRees <124360150+JuliaRees@users.noreply.github.com>
2023-10-13 11:49:22 +0800

    click-scroll

--- scripts.js
+++ scripts.js
@@ -11,13 +11,17 @@ $(document).ready(function() {
 var allPanels = $('.accordion > .dd').hide();
 
 	$('.accordion > .dt > a').click(function() {
-		allPanels.slideUp();
+	//	allPanels.slideUp();
 
-    $(this).filter('.open').removeClass('open').addClass('closing')
-    $('.accordion > .dt > a').removeClass('open');
-  $(this).not(".closing").addClass('open').parent().next().slideDown();
-    $('.accordion > .dt > a').removeClass('closing');
+        
+   // $(this).filter('.open').removeClass('open').addClass('closing')
+   // $('.accordion > .dt > a').removeClass('open');
+ // $(this).not(".closing").addClass('open').parent().next().slideDown();
+   // $('.accordion > .dt > a').removeClass('closing');
     
+   $(this).toggleClass('open')
+   $(this).parent().next().slideToggle() 
+
     return false;
 	});
 
